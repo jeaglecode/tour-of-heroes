4f4f95de7be37daca58db80407ae5672ef81729b
Jesse Duenas <jesserduenas@yahoo.com>
2023-03-11 02:38:09 -0800

    added some more code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-import {Hero} from "./hero";
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
 
-  value = 1.23;
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-
-hero: Hero = {
-  id: 1,
-  name: 'Big Boy'
-
-}
-
-
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
