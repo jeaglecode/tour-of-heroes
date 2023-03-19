efcd2f93cc945ee1a30df857a94f6c39eb1b63f0
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2023-03-18 22:34:20 -0700

    Finished messages for tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService} from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +15,7 @@ export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-constructor(private heroService: HeroService) {
+constructor(private heroService: HeroService, private messageService: MessageService) {
 }
 
   ngOnInit(): void {
@@ -23,13 +24,15 @@ constructor(private heroService: HeroService) {
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`You Selected Hero with id of ${hero.id} and name ${hero.name}`);
+    console.log(hero)
     this.selectedHero = hero;
   }
   getHeroes(): void {
   this.heroService.getHeroes()
-    .subscribe(data => {
-      console.log('got data')
-      this.heroes = data
+    .subscribe(x => {
+      console.log(x)
+      this.heroes = x
     })
   }
 
